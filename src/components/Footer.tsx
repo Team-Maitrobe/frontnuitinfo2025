4f3b0538c7a2b249dd258ddefa6a1016c8f3dc41
Octave <octave.raynaud@etu.univ-lyon1.fr>
2025-12-04 22:07:38 +0100

    finalisation de la page d'accueil, ajout d'un package npm pour les transitions

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="w-full bg-[#EF5B5B] py-4 flex justify-center items-center">
-      <p className="text-white text-sm">
+    <footer className="w-full bg-[#2E7D32] py-4 flex justify-center items-center shadow-inner">
+      <p className="text-white text-sm text-center">
         © 2025 Nuit de l'Info. Tous droits réservés.
       </p>
     </footer>
