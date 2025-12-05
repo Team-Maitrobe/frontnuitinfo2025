0d50492ff14e80c9bf6e0971211537b054193c59
xav <xavier.stoa@gmail.com>
2025-12-05 04:27:07 +0100

    Visual changes, added repo link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="w-full bg-[#2E7D32] py-4 flex justify-center items-center shadow-inner">
       <p className="text-white text-sm text-center">
-        © 2025 MaitroBé Org. Full Open Source !!
+        © 2025 MaitroBé Org. Full Open Source !! <a href="https://github.com/Team-Maitrobe/frontnuitinfo2025">(lien du repo)</a>
       </p>
     </footer>
   );
